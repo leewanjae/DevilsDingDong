2a48da11af380535123de12a09182def2d637b46
leewanjae <dhkswo1@gmail.com>
2024-06-25 12:32:55 +0900

    [fix] 오늘의 경기 UI수정 및 전체 파일 코드 정리

--- DevilsDingDong/View/TodayMatch/TodayMatchView.swift
+++ DevilsDingDong/View/TodayMatch/TodayMatchView.swift
@@ -17,7 +17,7 @@ class TodayMatchView: UIViewController {
         label.textAlignment = .center
         return label
     }()
-    lazy var MatchStackView: UIStackView = {
+    lazy var matchStackView: UIStackView = {
         let view = UIStackView()
         view.axis = .horizontal
         view.spacing = 10
@@ -27,7 +27,7 @@ class TodayMatchView: UIViewController {
     lazy var manUtdLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = UIFont.systemFont(ofSize: 24)
+        label.font = UIFont.systemFont(ofSize: 24, weight: .semibold)
         label.text = "맨유"
         return label
     }()
@@ -52,23 +52,78 @@ class TodayMatchView: UIViewController {
     lazy var enemyLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = UIFont.systemFont(ofSize: 24)
+        label.font = UIFont.systemFont(ofSize: 24, weight: .semibold)
+        return label
+    }()
+    lazy var dateStackView: UIStackView = {
+        let view = UIStackView()
+        view.axis = .horizontal
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    lazy var matchDate: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 20)
+        return label
+    }()
+    lazy var matchTime: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 20)
+        return label
+    }()
+    lazy var matchTypeStackView = {
+        let view = UIStackView()
+        view.axis = .horizontal
+        view.spacing = 10
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    lazy var matchType: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .gray
+        return label
+    }()
+    lazy var round: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .gray
+        return label
+    }()
+    lazy var stadium: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .gray
         return label
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .bgColor
-        if viewModel.todayMatch.first?.date != nil {
+        if viewModel.todayMatch.first?.enemy != nil {
             setUI()
         } else {
             setNoMatchUI()
         }
     }
-    
+}
+
+extension TodayMatchView {
     func setUI() {
         navigationItem.title = "오늘의 경기"
-        enemyLabel.text = viewModel.todayMatch.first?.enemy ?? "없음"
+        navigationItem.largeTitleDisplayMode = .always
+        navigationController?.navigationBar.prefersLargeTitles = true
+        matchType.text = viewModel.todayMatch.first?.matchType ?? "프리미어리그"
+        round.text = viewModel.todayMatch.first?.round ?? "0R"
+        stadium.text = viewModel.todayMatch.first?.stadium ?? "Old Trafford"
+        enemyLabel.text = viewModel.todayMatch.first?.enemy ?? "미정"
+        matchDate.text = viewModel.todayMatch.first?.date ?? "00년 00월 00일 (E)"
+        matchTime.text = viewModel.todayMatch.first?.time ?? "00:00"
         addView()
         setAutoLayout()
     }
@@ -82,20 +137,40 @@ class TodayMatchView: UIViewController {
     }
     
     func addView() {
-        view.addSubview(MatchStackView)
+        view.addSubview(matchStackView)
+        view.addSubview(stadium)
+        view.addSubview(dateStackView)
+        view.addSubview(matchTypeStackView)
         
-        MatchStackView.addArrangedSubview(manUtdLabel)
-        MatchStackView.addArrangedSubview(manUtdImage)
-        MatchStackView.addArrangedSubview(vsLabel)
-        MatchStackView.addArrangedSubview(enemyImage)
-        MatchStackView.addArrangedSubview(enemyLabel)
+        matchStackView.addArrangedSubview(manUtdLabel)
+        matchStackView.addArrangedSubview(manUtdImage)
+        matchStackView.addArrangedSubview(vsLabel)
+        matchStackView.addArrangedSubview(enemyImage)
+        matchStackView.addArrangedSubview(enemyLabel)
+        
+        dateStackView.addArrangedSubview(matchDate)
+        dateStackView.addArrangedSubview(matchTime)
+        
+        matchTypeStackView.addArrangedSubview(matchType)
+        matchTypeStackView.addArrangedSubview(round)
     }
     
     func setAutoLayout() {
         let safeArea = view.safeAreaLayoutGuide
         NSLayoutConstraint.activate([
-            MatchStackView.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 150),
-            MatchStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            matchStackView.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 20),
+            matchStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            matchTypeStackView.topAnchor.constraint(equalTo: matchStackView.bottomAnchor, constant: 10),
+            matchTypeStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            stadium.topAnchor.constraint(equalTo: matchTypeStackView.bottomAnchor, constant: 5),
+            stadium.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            dateStackView.topAnchor.constraint(equalTo: stadium.bottomAnchor, constant: 20),
+            dateStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+           
             
         ])
     }
